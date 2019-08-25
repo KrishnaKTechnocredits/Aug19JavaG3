8160b9e380f70d8bc1a1117c317954dba2bca744
Neha <neha_lodha2003@yahoo.com>
2019-08-25 09:22:46 +0530

    formatting

--- src/Neha/sumOfTwoInteger.java
+++ src/Neha/sumOfTwoInteger.java
@@ -1,17 +1,13 @@
 package Neha;
 
-
- public class sumOfTwoInteger{
-	 static void sum()
-	 {
-		 int a=5, b=7;
-			System.out.println("sum="+ (a+b));
-	 }
-
-public static void main(String[] args) {
-	
-	sum();
+public class sumOfTwoInteger {
+	static void sum() {
+		int a = 5, b = 7;
+		System.out.println("sum=" + (a + b));
 	}
- }
- 
 
+	public static void main(String[] args) {
+
+		sum();
+	}
+}
