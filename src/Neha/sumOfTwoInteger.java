d838e985650bacd7ea01573f90f372a0d2b4beb4
Neha <neha_lodha2003@yahoo.com>
2019-08-25 09:19:39 +0530

    sum of two integer

---
+++ src/Neha/sumOfTwoInteger.java
@@ -0,0 +1,17 @@
+package Neha;
+
+
+ public class sumOfTwoInteger{
+	 static void sum()
+	 {
+		 int a=5, b=7;
+			System.out.println("sum="+ (a+b));
+	 }
+
+public static void main(String[] args) {
+	
+	sum();
+	}
+ }
+ 
+
