32c797a27fd7fdbe3de180948379082cd56e0a10
Neha <neha_lodha2003@yahoo.com>
2019-09-08 09:00:32 +0530

    updated with scanner class

--- src/Neha/SumOfPair.java
+++ src/Neha/SumOfPair.java
@@ -1,8 +1,10 @@
 package Neha;
 
+import java.util.Scanner;
+
 public class SumOfPair {
-	static void sum() {
-		int[] a = { 2, 4, 3, 5, 7, 8, 9 };
+	static void sum(int[] a) {
+		//int[] a = { 2, 4, 3, 5, 7, 8, 9 };
 
 		for (int i = 0; i < a.length; i++) {
 			for (int j = i; j < a.length; j++) {
@@ -16,6 +18,14 @@ public class SumOfPair {
 	}
 
 	public static void main(String[] args) {
-		sum();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of Array: ");
+		int size = sc.nextInt();
+		System.out.println("Enter the elements for the array: ");
+		int[] array = new int[size];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
+		}
+		sum(array);
 	}
 }
