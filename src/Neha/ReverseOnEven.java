64e2ef1d03596b37399b11a3afd02a9b14c1e8d8
Neha <neha_lodha2003@yahoo.com>
2019-09-11 00:44:31 +0530

    Adding 2 more

---
+++ src/Neha/ReverseOnEven.java
@@ -0,0 +1,22 @@
+package Neha;
+
+public class ReverseOnEven {
+	static void reverse() {
+		String s = "this is techno credits";
+		String rev = "";
+		String[] str = s.split(" ");
+		for (int i = 0; i < str.length; i++) {
+			if (i % 2 == 0){
+				for (int j = str[i].length() - 1; j >= 0; j--) {
+					rev = rev + str[i].charAt(j);
+				}System.out.print(rev+ " ");rev = "";
+			}
+			else {System.out.print(str[i]+ " ");
+			}
+		}
+	}
+	public static void main(String[] args) {
+		reverse();
+	}
+
+}
