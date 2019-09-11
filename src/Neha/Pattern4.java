592513fee7ec688f338886ed98d154b5bc7c5eef
Neha <neha_lodha2003@yahoo.com>
2019-09-11 21:17:09 +0530

    Adding 8 programs

---
+++ src/Neha/Pattern4.java
@@ -0,0 +1,15 @@
+package Neha;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+
+		for (int i = 4; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
