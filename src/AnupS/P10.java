6173361473fab4c4e2d0f2016c369b8302a49184
Anup <sahoo.anupkumar@gmail.com>
2019-09-12 23:55:18 +0530

    Merge Conflict P10

---
+++ src/AnupS/P10.java
@@ -0,0 +1,16 @@
+package AnupS;
+//Pattern P4 question
+public class P10 {
+	public static void main(String[] args) {
+		int num = 5;
+		for (int i = 0;i<=num;i++)
+		{
+			for(int j=1;j<=num-i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
